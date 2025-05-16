7dcdc755ac9cab2fe65e9a9fe690b9cafbeadfc0
Esteban Ignacio Cantos <estebanignaci6@gmail.com>
2025-05-17 00:11:18 +0200

    Prueba de fuego pipelie ...

--- lambda/correct_exams/lambda_function.py
+++ lambda/correct_exams/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+    # Se hace correctamente el despliegue de la función lambda con el pipeline
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
